3f1d8190955ffd2f9aca15adfcf936854cd545eb
IT20181406 <it20181406@my.sllit.lk>
2021-10-07 12:37:58 +0530

    last

--- client/src/components/body/profile/Profile.js
+++ client/src/components/body/profile/Profile.js
@@ -294,7 +294,7 @@ function Profile() {
                                              onClick = {() => handleDelete(user._id)}></i>
                                         </td> 
                                      
-                                </tr>
+                                </tr> 
 
                                  ))
                              }
